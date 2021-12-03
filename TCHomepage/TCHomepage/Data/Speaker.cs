c8298ae7f7959d39434c32dfadacbfe2b0ed9b62
Max <max.luehr@campus-community.org>
2021-12-03 16:48:08 +0100

    Blazor Project creation

---
+++ TCHomepage/TCHomepage/Data/Speaker.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace TCHomepage.Data
+{
+    public class Speaker
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Organization { get; set; }
+
+        public string Bio { get; set; }
+
+        public string Picture { get; set; }
+
+        public string Website { get; set; }
+
+        public string Facebook { get; set; }
+
+        public string Twitter { get; set; }
+
+        public string Xing { get; set; }
+
+        public string LinkedIn { get; set; }
+    }
+}
